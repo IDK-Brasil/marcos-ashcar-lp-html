f459fb109f6786755d1da1ace437bca97e715e0b
matheusIDK <matheus.roberson@idkbrasil.com>
2026-01-08 08:07:38 -0300

    fix: remove navbar and change logos

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
-// MOBILE MENU
-(function () {
-  const hamburger = document.querySelector(".hamburger");
-  const closeMenuBtn = document.querySelector(".close-menu");
-  const overlay = document.getElementById("overlay");
-  const mobileMenu = document.getElementById("menu-mobile");
-  const mobileLinks = mobileMenu.querySelectorAll("a");
-
-  function toggleMenu(forceClose) {
-    const isOpen = hamburger.classList.contains("is-open");
-    const shouldOpen = forceClose ? false : !isOpen;
-
-    hamburger.classList.toggle("is-open", shouldOpen);
-    overlay.classList.toggle("is-open", shouldOpen);
-    mobileMenu.classList.toggle("is-open", shouldOpen);
-
-    hamburger.setAttribute("aria-expanded", shouldOpen ? "true" : "false");
-    mobileMenu.setAttribute("aria-hidden", shouldOpen ? "false" : "true");
-  }
-
-  hamburger.addEventListener("click", () => toggleMenu(false));
-  closeMenuBtn.addEventListener("click", () => toggleMenu(true));
-  overlay.addEventListener("click", () => toggleMenu(true));
-  mobileLinks.forEach((link) =>
-    link.addEventListener("click", () => toggleMenu(true))
-  );
-})();
 
 // ANIMATED SECTIONS AND SLIDER
 (function () {
